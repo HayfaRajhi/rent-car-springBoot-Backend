8d0d148875e7e2b2895d14550e8c94d5b35ae203
Fedi Rajhi <fedirjh@gmail.com>
2024-04-16 01:29:01 +0100

    update

--- src/main/java/com/ci2hayfa/carrental/web/controllers/CarRentalController.java
+++ src/main/java/com/ci2hayfa/carrental/web/controllers/CarRentalController.java
@@ -5,10 +5,13 @@ import com.ci2hayfa.carrental.dao.entities.Vehicle;
 import com.ci2hayfa.carrental.exceptions.DuplicateVehicleException;
 import com.ci2hayfa.carrental.web.dto.VehicleDTO;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
